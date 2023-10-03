be58baa68a822496d0610051076401d3b6ce7917
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-10-04 00:56:52 +0200

    Update Notifications.js

--- client/src/pages/Notifications.js
+++ client/src/pages/Notifications.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Layout from "../Components/Layout";
-import { Tabs } from "antd";
+import { Tabs, notification } from "antd";
+import { useSelector } from "react-redux";
 
 function Notifications() {
+    const {user} = useSelector((state) => state.user)
     return (
         <Layout>
             <h1 className="page-title">Notifications</h1>
@@ -12,6 +14,13 @@ function Notifications() {
                     <div className="d-flex justify-content-end">
                         <h1 className="anchor">Mark all as seen</h1>
                     </div>
+                    {
+                        user.unseenNotifications.map((notification) => {
+                            <div className="card p-2">
+                                <div className="card-text">{notification.message}</div>
+                            </div>
+                        })
+                    }
                 </Tabs.TabPane>
                 <Tabs.TabPane tab='seen' key={1}>
                     <div className="d-flex justify-content-end">
